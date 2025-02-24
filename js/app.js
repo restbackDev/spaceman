fa9e9ffad3880763ed6ce36caeb03ad6b52a97db
restbackDev <jason.rbdev@gmail.com>
2025-02-24 13:40:32 -0800

    created index+data+app+style

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+// const button = document.querySelector(".button").addEventListener('click',display);
+
+//   button.style.display ='hide';
+// }
+// function display(event) {
+
+// /*
+
+// 1. add an event lisenter, to the their keyboard
+
+// 2. create a function to determine if the letter clicked or types matches
+//   if the letter picked matches with the hidden word
+//     the letter is displayed in a div of letters guessed
+//     the letter is displayed , games continues
+//   else if the letter does not match the hidden word,
+//     the letter is displayed in a div of letters guessed
+//     and the rocket will (1) will be removes 
+  
+//   if there are no more rockets left
+//     player lost
+
+  
+
+// COM display a message if they WON or LOSS
+// wrap a keys 
+
+// functions activates to the event listener
+
+
+
+
+  
+//     */
---
+++ js/data.js
@@ -0,0 +1,24 @@
+// const country = [
+//   {name: "Albania"},
+//   {name: "Afghanistan"},
+//   {name: "Algeria"},
+//   {name: "American Samoa"},
+//   {name: "Andorra"},
+//   {name: "Angola"},
+//   {name: "Anguilla"},
+//   {name: "Antarctica"},
+//   {name: "Antigua and Barbuda"},
+//   {name: "Argentina"},
+//   {name: "Armenia"},
+//   {name: "Aruba"},
+//   {name: "Australia"},
+//   {name: "Austria"},
+//   {name: "Azerbaijan"},
+// ];
+
+const countryName = {
+  name: "Albania",
+  name: "Afghanistan",
+};
+
+module.exports = countryName;
